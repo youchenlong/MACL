6383a3a9d5c604c3bccd7f26b76863535b4ab3ac
youchenlong <2091838306@qq.com>
2024-05-30 23:11:42 +0800

    fix a bug: CUDA out of memory in Predator-Prey and MMM2

--- src/utils/contrastive.py
+++ src/utils/contrastive.py
@@ -1,6 +1,6 @@
 import torch as th
 
-def infoNCE_loss(features, positive_mask, contrasTemperature):
+def infoNCE_loss(features, positive_mask, contrasTemperature, batch_size=512):
     """
     features: [bs, feature_dim]
     positive_mask: [bs, bs]
@@ -10,18 +10,27 @@ def infoNCE_loss(features, positive_mask, contrasTemperature):
         \end{aligned}
     \end{equation*}
     """
-    # similarity matrix
-    sim_matrix = th.matmul(features, features.T) / contrasTemperature
-    # Stability trick: Subtract the maximum value for numerical stability
-    logits_max, _ = th.max(sim_matrix, dim=1, keepdim=True)
-    logits = sim_matrix - logits_max.detach()
-    # negative logits
-    exp_logits = th.exp(logits)
-    exp_sum = exp_logits.sum(1, keepdim=True) - exp_logits * positive_mask
-    # positive logits
-    log_prob = logits - th.log(exp_sum)
-    # mask out the negative samples
-    log_prob_pos = (positive_mask * log_prob).sum(1) / positive_mask.sum(1)
-    # contrastive loss
-    contrastive_loss = -log_prob_pos.mean()
-    return contrastive_loss
+    bs, feature_dim = features.size()
+    total_loss = 0
+    n_batchs = (bs + batch_size - 1) // batch_size
+    for i in range(n_batchs):
+        start = i * batch_size
+        end = min(start + batch_size, bs)
+        features_batch = features[start:end]
+        # similarity matrix
+        sim_matrix = th.matmul(features_batch, features.T) / contrasTemperature
+        # Stability trick: Subtract the maximum value for numerical stability
+        logits_max, _ = th.max(sim_matrix, dim=1, keepdim=True)
+        logits = sim_matrix - logits_max.detach()
+        # negative logits
+        exp_logits = th.exp(logits)
+        exp_sum = exp_logits.sum(1, keepdim=True) - exp_logits * positive_mask[start:end]
+        # positive logits
+        log_prob = logits - th.log(exp_sum)
+        # mask out the negative samples
+        positive_mask_batch = positive_mask[start:end]
+        log_prob_pos = (positive_mask_batch * log_prob).sum(1) / positive_mask_batch.sum(1)
+        # contrastive loss
+        total_loss += -log_prob_pos.mean()
+    total_loss /= n_batchs
+    return total_loss
