fix a bug: numerically unstable problem in contrastive loss

src/modules/cb/consensus_builder.py
@@ -24,26 +24,25 @@ class ConsensusBuilder(nn.Module):
         )
         # nonlinear projector
         self.online_projector = nn.Sequential(
-            nn.Linear(self.args.rnn_hidden_dim, self.args.consensus_hidden_dim),
+            nn.Linear(self.args.rnn_hidden_dim, self.args.consensus_dim * 4),
             nn.ReLU(),
-            nn.Linear(self.args.consensus_hidden_dim, self.args.consensus_dim)
+            nn.Linear(self.args.consensus_dim * 4, self.args.consensus_dim)
         )
         # predictor is similar to projector
         self.predictor = nn.Sequential(
-            nn.Linear(self.args.consensus_dim, self.args.consensus_hidden_dim), 
+            nn.Linear(self.args.consensus_dim, self.args.consensus_dim * 4), 
             nn.ReLU(), 
-            nn.Linear(self.args.consensus_hidden_dim, self.args.consensus_dim)
+            nn.Linear(self.args.consensus_dim * 4, self.args.consensus_dim)
         )
 
         # similar to online encoder and online projector, but update without gradient
         self.target_encoder = copy.deepcopy(encoder)
         self.target_projector = nn.Sequential(
-            nn.Linear(self.args.rnn_hidden_dim, self.args.consensus_hidden_dim),
+            nn.Linear(self.args.rnn_hidden_dim, self.args.consensus_dim * 4),
             nn.ReLU(),
-            nn.Linear(self.args.consensus_hidden_dim, self.args.consensus_dim)
+            nn.Linear(self.args.consensus_dim * 4, self.args.consensus_dim)
         )
 
-        # self.tau = args.tau
         # tau increase with training steps
         self.schedule = LinearSchedule(args.tau_start, args.tau_finish, args.t_max, decay="linear")
         self.tau = self.schedule.eval(0)

src/test.py
@@ -5,12 +5,13 @@ import torch.nn.functional as F
 from utils.contrastive import ContrastiveLoss
 from components.epsilon_schedules import LinearSchedule
 
-# bs = 128
-# ts = 200
-# n_agents = 8
-# consensus_dim = 4
-# loss_func = ContrastiveLoss(ts)
-# loss = th.tensor(0.0).cuda()
+bs = 128
+ts = 200
+n_agents = 8
+consensus_dim = 4
+loss_func = ContrastiveLoss(ts)
+loss = th.tensor(0.0).cuda()
+
 # start_time = time.time()
 # prediction = th.rand(bs, ts, n_agents * consensus_dim).cuda()
 # target_projection = th.rand(bs, ts, n_agents * consensus_dim).cuda()
@@ -19,19 +20,20 @@ from components.epsilon_schedules import LinearSchedule
 # end_time = time.time()
 # print("cost time: ", end_time - start_time)
 # print(loss/(bs*ts))
-# start_time = time.time()
-# emb_i = th.zeros(ts, n_agents * consensus_dim).cuda()
-# emb_j = th.zeros(ts, n_agents * consensus_dim).cuda()
-# loss = loss_func(emb_i, emb_j)
-# end_time = time.time()
-# print("cost time: ", end_time - start_time)
-# print(loss)
+
+start_time = time.time()
+emb_i = th.zeros(ts, n_agents * consensus_dim).cuda()
+emb_j = th.zeros(ts, n_agents * consensus_dim).cuda()
+loss = loss_func(emb_i, emb_j)
+end_time = time.time()
+print("cost time: ", end_time - start_time)
+print(loss)
 
 
-tau_start = 0.99
-tau_finish = 1.0
-t_max = 2050000
-schedule = LinearSchedule(tau_start, tau_finish, t_max, decay="linear")
-for t_env in range(t_max):
-    if t_env % 10000 == 0:
-        print(schedule.eval(t_env))
+# tau_start = 0.99
+# tau_finish = 1.0
+# t_max = 2050000
+# schedule = LinearSchedule(tau_start, tau_finish, t_max, decay="linear")
+# for t_env in range(t_max):
+#     if t_env % 10000 == 0:
+#         print(schedule.eval(t_env))

src/utils/contrastive.py
@@ -25,8 +25,12 @@ class ContrastiveLoss(nn.Module):
         sim_ij = th.diag(similarity_matrix, self.batch_size) # [bs]
         sim_ji = th.diag(similarity_matrix, -self.batch_size) # [bs]
         positives = th.cat([sim_ij, sim_ji], dim=0) # [2 * bs]
-        nominator = th.exp(positives / self.temperature) # [2 * bs]
-        denominator = th.exp(similarity_matrix / self.temperature) * self.negative_mask # [2 * bs, 2 * bs]
-        loss_partial = -th.log(nominator / th.sum(denominator, dim=1)) # [2 * bs]
+
+        # nominator = th.exp(positives / self.temperature) # [2 * bs]
+        # denominator = th.exp(similarity_matrix / self.temperature) * self.negative_mask # [2 * bs, 2 * bs]
+        # loss_partial = -th.log(nominator / th.sum(denominator, dim=1)) # [2 * bs]
+
+        # TODO: mask out the diagonal
+        loss_partial = -th.log(th.exp(positives / self.temperature)) + th.logsumexp(similarity_matrix / self.temperature, dim=1) # [2 * bs]
         loss = th.sum(loss_partial) / (2 * self.batch_size)
         return loss