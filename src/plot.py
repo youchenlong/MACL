af6381ff637abc88d44cd93b8f3a350936df4015
youchenlong <2091838306@qq.com>
2024-06-16 18:35:39 +0800

    add plot file

---
+++ src/plot.py
@@ -0,0 +1,30 @@
+import os
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+def plot(filename):
+    with open(filename, 'r', encoding='utf-8') as f:
+        data =json.load(f)
+
+    if "simple_spread" in filename or "lbf" in filename:
+        metric = [item["value"] for item in data["test_return_mean"]]
+    else:
+        metric = data["test_battle_won_mean"]
+
+    plt.plot(metric)
+    plt.show()
+
+def main():
+    dir_name = os.path.join(os.getcwd(), "results/sacred")
+    alg_name = "macl"
+    map_name = "3s_vs_5z"
+    t = "1"
+    filename = os.path.join(dir_name, alg_name, map_name, t, "info.json")
+
+    plot(filename)
+
+if __name__ == "__main__":
+    main()
